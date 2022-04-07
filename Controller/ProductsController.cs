f67522a82dacc98277b8f9d581851f9409c8b922
Willian Alberto Lauber <wlauber@pm.me>
2022-04-07 16:24:05 -0300

    feat

--- Controller/ProductsController.cs
+++ Controller/ProductsController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Shop.API.Repository;
 using Shop.API.Models;
 
@@ -36,7 +37,64 @@ namespace Shop.API.Controllers
             }
             return Ok(result);
         }
+        
+        [HttpPost]
+        public async Task<ActionResult> CreateProduct(Product product)
+        {
+            try
+            {
+                _context.Products.Add(product);
+                await _context.SaveChangesAsync();
+                return StatusCode(201);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return StatusCode(500);
+            }
+        }
 
+        [HttpDelete]
+        public async Task<ActionResult> DeleteProduct(int id)
+        {   try{
+                var product = await _context.Products.FindAsync(id);
+                if (product == null)
+                {
+                    return StatusCode(404, "Not Found");
+                }
+                _context.Products.Remove(product);
+                _context.SaveChangesAsync();
+                return StatusCode(200);
+            }
+            catch
+            {
+                return StatusCode(404, "Sorry,");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ObjectResult> UpdateProduct(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return StatusCode(400, "Not the same product");
+
+            }
+            if (await _context.Products.FindAsync(id) == null)
+            {
+                return StatusCode(400, "Not found");
+            }
+            // _context.Entry(product).State = EntityState.Modified;
+            _context.Products.Update(product);
+            try
+            {
+                await _context.SaveChangesAsync();
+                return StatusCode(200, product);
+            }catch(System.Exception)
+            {
+                return StatusCode(500, "Something went wrong");
+            }
+        }
 
 
     }
