4ae41a516814e5839793327fc3b031df994c7564
mawavial <panzerkovic@gmail.com>
2022-04-25 15:51:59 -0300

    adds cors into backend calls

--- API/Business/CrateBusiness.cs
+++ API/Business/CrateBusiness.cs
@@ -13,8 +13,7 @@ public class CrateBusiness
 
     public Crate AddToCart(Crate crate)
     {
-
-        return _CrateRepository.AddToCart(crate);
+        return crate;
     }
 
 }
--- API/Program.cs
+++ API/Program.cs
@@ -7,11 +7,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<IProductBusiness, ProductBusiness>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
+
+
 builder.Services.AddControllers();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("corsapp", policy =>
+    {
+        policy.WithOrigins("*")
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
 
 
 builder.Services.AddDbContext<ShopContext>(options =>
@@ -29,8 +41,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseCors("corsapp");
 
+app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
